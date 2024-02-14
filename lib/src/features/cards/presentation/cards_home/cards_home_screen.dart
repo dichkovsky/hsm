c9c7693642b3778632dacac2b13da25033882040
Dmitry Dichkovski <ddichkovskiy@gmail.com>
2024-02-14 16:50:13 -0500

    Crads home screen - added card of the day preview and random cards daily count

--- lib/src/features/cards/presentation/cards_home/cards_home_screen.dart
+++ lib/src/features/cards/presentation/cards_home/cards_home_screen.dart
@@ -1,15 +1,36 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hsm/src/common_widgets/ui_card.dart';
 import 'package:hsm/src/constants/app_sizes.dart';
+import 'package:hsm/src/features/cards/application/card_of_the_day_service.dart';
+import 'package:hsm/src/features/cards/application/random_card_service.dart';
 import 'package:hsm/src/system/localization/app_localizations_context.dart';
 import 'package:hsm/src/system/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class CardsHomeScreen extends StatelessWidget {
+class CardsHomeScreen extends ConsumerWidget {
   const CardsHomeScreen({super.key});
 
+
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final randomCardsLeft = ref.watch(randomCardsDrawsLeftProvider);
+
+    var randomCardSubtitle = context.loc.randomCardDesc;
+    if (randomCardsLeft >= 0) {
+      randomCardSubtitle = "$randomCardSubtitle ${context.loc.randomCardDrawsLeft(randomCardsLeft)}";
+    }
+
+    final lastCardOfTheDay = ref.watch(lastCardOfTheDayProvider);
+    var isCardOrTheDayAvailable = false;
+    var cardOfTheDayImg = 'assets/elements/CardOfTheDay.png';
+    if (lastCardOfTheDay == null) {
+      isCardOrTheDayAvailable = true;
+    } else {
+      cardOfTheDayImg = lastCardOfTheDay.faceURL;
+    }
+
     return Container(
       alignment: Alignment.center,
       child: ListView(
@@ -17,11 +38,20 @@ class CardsHomeScreen extends StatelessWidget {
         children: [
           UICard(
             height: Sizes.p104,
+            title: context.loc.cardOfTheDay,
+            titleColor: isCardOrTheDayAvailable ? Theme.of(context).colorScheme.background : null,
+            bgColor: isCardOrTheDayAvailable ? Theme.of(context).colorScheme.primary : null,
+            subTitle: context.loc.cardOfTheDayDesc,
+            subTitleColor: isCardOrTheDayAvailable ? Theme.of(context).colorScheme.background : null,
+            bgImage: cardOfTheDayImg,
+            onTap: () {
+              context.goNamed(AppRoutes.cardOfTheDay.name);
+            },
+          ),
+          UICard(
+            height: Sizes.p104,
             title: context.loc.randomCard,
-            titleColor: Theme.of(context).colorScheme.background,
-            bgColor: Theme.of(context).colorScheme.primary,
-            subTitle: context.loc.randomCardDesc,
-            subTitleColor: Theme.of(context).colorScheme.background,
+            subTitle: randomCardSubtitle,
             bgImage: 'assets/elements/CardOfTheDay.png',
             onTap: () {
               context.goNamed(AppRoutes.randomCard.name);
